ed664adfed76a8b21475e5695195ee485f021864
plux <plux@parasit.(none)>
2013-08-15 03:04:37 +0200

    Bump version

--- ordjakt.py
+++ ordjakt.py
@@ -57,5 +57,5 @@ def is_palindrome(word):
     return word == word[::-1]
 
 if __name__ == "__main__":
-    args = docopt.docopt(__doc__, version='Ordjakt 1.0.0')
+    args = docopt.docopt(__doc__, version='Ordjakt 1.1.0')
     main(args)
